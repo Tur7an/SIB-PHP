b6b31ddbb9429e57b145e083dada2e2bd0a58d6e
tur7an <faturrahmanardiansyah17@gmail.com>
2024-03-29 02:15:11 +0700

    Ini adalah tugas 3 Pemrograman PHP Fatur

--- Tugas 3/index.php
+++ Tugas 3/index.php
@@ -110,5 +110,6 @@ $ar_nilai = [$a1,$a2,$a3,$a4,$a5,$a6,$a7,$a8,$a9,$a10];
             <?php } ?>
         </tfoot>
     </table>
+    <footer> <center>&copy; <?= date('Y') ?> Fatur Rahman | Group 1</footer>
 </body>
 </html>
